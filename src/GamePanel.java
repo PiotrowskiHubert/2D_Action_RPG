10cdbbb38444c294688d1d207c78f42c239d1f10
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2022-12-09 19:19:14 +0100

    moving block by using wsad

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -37,29 +37,36 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     @Override
-    public void run() {
-        double drawInterval = 1000000000/FPS; //0.01666 sec
-        double nextDrawTime= System.nanoTime()+drawInterval;
-
+//    public void run() {
+//        double drawInterval = 1000000000/FPS; //0.01666 sec
+//        double nextDrawTime= System.nanoTime()+drawInterval;
+//
+//        while(gameThread!=null){
+//
+//            update();
+//
+//            repaint();
+//
+//            try {
+//                double remainingTime = nextDrawTime-System.nanoTime();
+//                remainingTime=remainingTime/1000000;
+//
+//                if(remainingTime<0){
+//                    remainingTime=0;
+//                }
+//
+//                Thread.sleep((long) remainingTime);
+//                nextDrawTime+=drawInterval;
+//            }catch (InterruptedException e){
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+    public void run(){
+        double drawInterval = 1000000000/FPS;
         while(gameThread!=null){
-
-            update();
-
-            repaint();
-
-            try {
-                double remainingTime = nextDrawTime-System.nanoTime();
-                remainingTime=remainingTime/1000000;
-
-                if(remainingTime<0){
-                    remainingTime=0;
-                }
-
-                Thread.sleep((long) remainingTime);
-                nextDrawTime+=drawInterval;
-            }catch (InterruptedException e){
-                e.printStackTrace();
-            }
+         update();
+         repaint();
         }
     }
     public void update(){
