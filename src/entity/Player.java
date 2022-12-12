31fcedb9515a894f790841db83efa786f4842f91
Hubert <hubert.piotrowski.kmwtw@gmail.com>
2022-12-12 21:10:10 +0100

    smthn wrong

---
+++ src/entity/Player.java
@@ -0,0 +1,140 @@
+package entity;
+
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Player extends Entity{
+
+    GamePanel gp;
+    KeyHandler keyH;
+    public final int screenX;
+    public final int screenY;
+        public Player(GamePanel gp, KeyHandler keyH){
+
+            this.gp = gp;
+            this.keyH= keyH;
+
+            screenX=(gp.screenWidth/2)-(gp.tileSize/2);
+            screenY=(gp.screenHeight/2)-(gp.tileSize/2);
+
+            SetDefaultValues();
+            getPlayerImage();
+        }
+        public void SetDefaultValues(){
+            worldX=gp.tileSize*23;
+            worldY=gp.tileSize*21;
+            speed=4;
+            direction="down";
+        }
+        public void getPlayerImage(){
+
+            try {
+                up1= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_up_1.png"));
+                up2= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_up_2.png"));
+                down1= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_down_1.png"));
+                down2= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_down_2.png"));
+                right1= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_right_1.png"));
+                right2= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_right_2.png"));
+                left1= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_left_1.png"));
+                left2= ImageIO.read(getClass().getResourceAsStream("/resource/player/boy_left_2.png"));
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        public void update(){
+
+            if (keyH.upPressed || keyH.downPressed
+                    || keyH.leftPressed || keyH.rightPressed) {
+
+                if (keyH.upPressed) {
+                    direction = "up";
+                    worldY -= speed;
+                    //playerY=playerY-playerSpeed;
+                } else if (keyH.downPressed) {
+                    direction = "down";
+                    worldY += speed;
+                } else if (keyH.leftPressed) {
+                    direction = "left";
+                    worldX -= speed;
+                } else if (keyH.rightPressed) {
+                    direction = "right";
+                    worldX += speed;
+                }
+                spriteCounter++;
+
+                if (spriteCounter > 15) {
+                    if (spriteNum == 1) {
+                        spriteNum = 2;
+                    } else if (spriteNum == 2) {
+                        spriteNum = 1;
+                    }
+                    spriteCounter = 0;
+                }
+            }else {
+                spriteCounter++;
+
+                if (spriteCounter > 45) {
+                    if (spriteNum == 1) {
+                        spriteNum = 2;
+                    } else if (spriteNum == 2) {
+                        spriteNum = 1;
+                    }
+                    spriteCounter = 0;
+                }
+            }
+        }
+        public void draw(Graphics2D g2){
+//            g2.setColor(Color.white);
+//            g2.fillRect(x, y,gp.tileSize,gp.tileSize);
+
+            BufferedImage image = null;
+            switch (direction){
+                case "up":{
+                    if (spriteNum==1) {
+                        image = up1;
+                    }
+                    if (spriteNum==2) {
+                        image = up2;
+                    }
+                    break;
+                }
+                case "down": {
+                    if (spriteNum == 1) {
+                        image = down1;
+                    }
+                    if (spriteNum == 2) {
+                    image = down2;
+                    }
+                    break;
+                }
+                case "left":{
+                    if (spriteNum==1) {
+                        image = left1;
+                    }
+                    if (spriteNum==2) {
+                        image = left2;
+                    }
+                    break;
+                }
+                case "right":{
+                    if (spriteNum==1) {
+                        image = right1;
+                    }
+                    if (spriteNum==2) {
+                        image = right2;
+                    }
+                    break;
+                }
+            }
+            g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+
+        }
+
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,3 +1,8 @@
+package main;
+
+import entity.Player;
+import tile.TileManager;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,19 +12,21 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize=16; //16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize*scale; //48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth=tileSize*maxScreenCol; // 768 pixels
-    final int screenHeight=tileSize*maxScreenRow;  // 576 pixels
+    public final int tileSize = originalTileSize*scale; //48x48 tile
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth=tileSize*maxScreenCol; // 768 pixels
+    public final int screenHeight=tileSize*maxScreenRow;  // 576 pixels
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
-
-    // player def positio
-    int playerX=100;
-    int playerY =100;
-    int playerSpeed=4;
+    public Player player = new Player(this,keyH);
     int FPS = 60;
+    TileManager tileM= new TileManager(this);
+    // WORLD SETTINGS
+    public final int maxWorldCol=50;
+    public final int maxWorldRow=50;
+    public final int worldWidth = tileSize*maxWorldCol;
+    public final int worldHeight = tileSize*maxWorldRow;
 
 
 
@@ -37,6 +44,7 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     @Override
+    // game loop #1
 //    public void run() {
 //        double drawInterval = 1000000000/FPS; //0.01666 sec
 //        double nextDrawTime= System.nanoTime()+drawInterval;
@@ -62,31 +70,46 @@ public class GamePanel extends JPanel implements Runnable{
 //            }
 //        }
 //    }
+
+    // game loop #2
     public void run(){
         double drawInterval = 1000000000/FPS;
+        double delta=0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer=0;
+        long drawCount=0;
+
         while(gameThread!=null){
-         update();
-         repaint();
+
+            currentTime=System.nanoTime();
+            delta+=(currentTime-lastTime)/drawInterval;
+            timer+=(currentTime-lastTime);
+            lastTime=currentTime;
+
+
+            if (delta>= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000){
+                System.out.println("FPS: "+drawCount);
+                drawCount=0;
+                timer=0;
+            }
         }
     }
     public void update(){
-        if(keyH.upPressed){
-            playerY-=playerSpeed;
-            //playerY=playerY-playerSpeed;
-        } else if (keyH.downPressed) {
-            playerY+=playerSpeed;
-        } else if (keyH.leftPressed) {
-            playerX-=playerSpeed;
-        } else if (keyH.rightPressed) {
-            playerX+=playerSpeed;
-        }
+        player.update();
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2=(Graphics2D)g;
-
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY,tileSize,tileSize);
+        tileM.draw(g2);
+        player.draw(g2);
         g2.dispose();
     }
 }
